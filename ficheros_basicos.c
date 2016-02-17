a7539b2a2d3aa892ef579b39ee6c21a43b3abd00
monigr25 <monigrancharov@hotmail.com>
2016-02-17 17:07:14 +0100

    Funciones en ficheros_basicos.c

--- ficheros_basicos.c
+++ ficheros_basicos.c
@@ -1,4 +1,5 @@
 #include "ficheros_basicos.h"
+#include "bloques.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,8 +7,29 @@ int main(){
 
 struct inodo ex;
 printf("El tamaño del fichero es: %lu", sizeof(ex));
-
+printf ("sizeof time_t is: %d\n", sizeof(time_t));
 return 0;
+}
+
+int tamMB(unsigned int nbloques){
+    int auxMB = nbloques/8;
+    if(auxMB % BLOCKSIZE != 0){
+       auxMB++;
+
+    }
+    return auxMB;
+}
+
+int tamAI(unsigned int ninodos){
+    int auxAI = ninodos*TAM_INODO;
+    if(auxAI % BLOCKSIZE){
+    auxAI++;
+    }
+    return auxAI;
+}
+
+int initSB(unsigned int nbloques, unsigned int ninodos){
+
 }
 
 

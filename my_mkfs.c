6830a108a4ce9f4355be75f77723b28021ca2a8a
jose122333 <josevela9458@gmailcom>
2016-02-17 17:20:23 +0100

    Correción my_mkfs

--- my_mkfs.c
+++ my_mkfs.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv){
 int numBlocks = atoi(argv[2]);
 int descriptor = bmount(argv[1]);
 unsigned char buf[BLOCKSIZE];
-memset(buf,0,sizeof(numBlocks*BLOCKSIZE));
+memset(buf,0, BLOCKSIZE);
 int i;
 for(i=0; i<numBlocks; i++){
 bwrite(i,buf);
